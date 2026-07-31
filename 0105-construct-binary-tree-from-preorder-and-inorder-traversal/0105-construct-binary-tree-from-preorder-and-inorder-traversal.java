5aac475400992553702fcf40cef8de53323bf3a5
KrishnaBhardwaj1304 <bhardwajkrishna1304@gmail.com>
2026-07-31 13:47:13 +0530

    Time: 3 ms (29.08%), Space: 45 MB (98.52%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,31 +1,38 @@
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private Map<Integer, Integer> inorderIndex = new HashMap<>();
-    private int preorderPointer = 0;
+    public TreeNode helper(int[] preorder , int prelow , int prehi , int[] inorder , int inlow , int inhi){
+        if(prelow > prehi) return null;
+        TreeNode root = new TreeNode(preorder[prelow]);
+        int i = inlow;
+        while(inorder[i] != root.val)i++;
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        for (int i = 0; i < inorder.length; i++) {
-            inorderIndex.put(inorder[i], i);
-        }
+        int leftsize = i - inlow;
 
-        return build(preorder, 0, inorder.length - 1);
+        root.left = helper(preorder, prelow+1, prelow + leftsize, inorder, inlow , i-1);
+        root.right = helper(preorder,prelow+leftsize+1, prehi , inorder , i+1,inhi);
+        return root;
+
     }
 
-    private TreeNode build(int[] preorder, int left, int right) {
-        if (left > right) {
-            return null;
-        }
 
-        int rootValue = preorder[preorderPointer++];
-        TreeNode root = new TreeNode(rootValue);
 
-        int rootIndex = inorderIndex.get(rootValue);
 
-        root.left = build(preorder, left, rootIndex - 1);
-        root.right = build(preorder, rootIndex + 1, right);
-
-        return root;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        return helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
     }
 }
