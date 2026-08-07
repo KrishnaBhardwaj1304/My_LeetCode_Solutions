c4336d7405667dfdf50d3b6b21eb4ac0b9fe79a2
KrishnaBhardwaj1304 <bhardwajkrishna1304@gmail.com>
2026-08-08 00:09:08 +0530

    Time: 0 ms (100%), Space: 46.3 MB (70.02%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.java
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.java
@@ -1,56 +1,32 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode { 
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class Solution {
-//     public int calculate(TreeNode root, int ttt ){
-//         if(root == null) return 0;
-
-//         int ls = calculate(root.left);
-//         int rs = calculate(root.right);
-
-//         ttt += Math.abs(ls - rs);
-
-//         return ls + rs + root.val;
-//     }
-//     public int findTilt(TreeNode root) {
-//         int ttt = 0;
-//         calculte(root , ttt);
-//         return ttt;
-//     }
-// }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode { 
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int totalTilt = 0;
+    int ttt = 0;
+    public int calculate(TreeNode root){
+        if(root == null) return 0;
 
-    public int findTilt(TreeNode root) {
-        totalTilt = 0;
-        calculateSubtreeSum(root);
-        return totalTilt;
+        int ls = calculate(root.left);
+        int rs = calculate(root.right);
+
+        ttt += Math.abs(ls - rs);
+
+        return ls + rs + root.val;
     }
-
-    private int calculateSubtreeSum(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int leftSum = calculateSubtreeSum(node.left);
-        int rightSum = calculateSubtreeSum(node.right);
-
-        // Add the tilt of the current node to the total
-        totalTilt += Math.abs(leftSum - rightSum);
-
-        // Return total sum of subtree rooted at this node
-        return node.val + leftSum + rightSum;
+    public int findTilt(TreeNode root) {
+        calculate(root);
+        return ttt;
     }
 }
