4ea7a6d0c0867665e268d27d79261459a533a744
KrishnaBhardwaj1304 <bhardwajkrishna1304@gmail.com>
2026-08-09 00:17:26 +0530

    Time: 6 ms (8.73%), Space: 44.4 MB (98.9%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,58 +1,58 @@
-// class Solution {
-//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-//          int [] arr = new int[nums1.length];
-//          int k = 0;
-//          for(int i = 0 ; i < nums1.length ;i++){
-//              for(int j = 0 ; j < nums2.length ; j++){
-                
-//                 if(nums1[i] == nums2[j]){
-//                     k = j;
-//                 }
-//              }
-//                 if(k == nums2.length - 1){
-//                     arr[i] = -1;
-//                     break;
-//                 }
-
-                
-                
-//                     int a = nums1[i];
-//                     for(int l = k+1 ; l < nums2.length ; l ++){
-//                         if(nums2[l] > a){
-//                             arr[i] = nums2[l];
-//                             break;
-//                         }
-//                         else{
-//                             arr[i] = -1;
-//                         }
-                        
-//                     }
-//     }
-//           return arr;      
-//          }
-
-//     }
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> nextGreater = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+         int [] arr = new int[nums1.length];
+         int k = 0;
+         for(int i = 0 ; i < nums1.length ;i++){
+             for(int j = 0 ; j < nums2.length ; j++){
+                
+                if(nums1[i] == nums2[j]){
+                    k = j;
+                }
+             }
+                if(k == nums2.length - 1){
+                    arr[i] = -1;
+                    
+                }
 
-        for (int i = 0; i < nums2.length; i++) {
-            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
-                nextGreater.put(stack.pop(), nums2[i]);
-            }
-
-            stack.push(nums2[i]);
-        }
-
-        int[] answer = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            answer[i] = nextGreater.getOrDefault(nums1[i], -1);
-        }
-
-        return answer;
+                
+                
+                    int a = nums1[i];
+                    for(int l = k+1 ; l < nums2.length ; l ++){
+                        if(nums2[l] > a){
+                            arr[i] = nums2[l];
+                            break; 
+                        }
+                        else{
+                            arr[i] = -1;
+                        }
+                        
+                    }
     }
-}
+          return arr;      
+         }
+
+    }
+// import java.util.*;
+
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         HashMap<Integer, Integer> nextGreater = new HashMap<>();
+//         Stack<Integer> stack = new Stack<>();
+
+//         for (int i = 0; i < nums2.length; i++) {
+//             while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+//                 nextGreater.put(stack.pop(), nums2[i]);
+//             }
+
+//             stack.push(nums2[i]);
+//         }
+
+//         int[] answer = new int[nums1.length];
+
+//         for (int i = 0; i < nums1.length; i++) {
+//             answer[i] = nextGreater.getOrDefault(nums1[i], -1);
+//         }
+
+//         return answer;
+//     }
+// }
