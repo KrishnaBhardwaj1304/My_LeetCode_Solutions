e6dac078cd853843cf8773250105d33f34693e64
KrishnaBhardwaj1304 <bhardwajkrishna1304@gmail.com>
2026-09-09 23:56:37 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (56.41%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -2,11 +2,10 @@ class Solution {
     public long countCommas(long n) {
         long totalCommas = 0;
         
-        // Boundaries where commas are added: 10^3, 10^6, 10^9, 10^12, 10^15
+
         for (long boundary = 1000; boundary <= n; boundary *= 1000) {
             totalCommas += (n - boundary + 1);
-            
-            // Prevent potential overflow if boundary * 1000 exceeds Long.MAX_VALUE
+
             if (boundary > Long.MAX_VALUE / 1000) {
                 break;
             }
