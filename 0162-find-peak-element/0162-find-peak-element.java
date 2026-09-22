b5dfbad9e92ab4062561918c45aa8697adb55170
KrishnaBhardwaj1304 <bhardwajkrishna1304@gmail.com>
2026-09-22 15:07:17 +0530

    Time: 0 ms (100%), Space: 44.4 MB (20.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -19,38 +19,38 @@
 //     }
 
 
-// class Solution{
-//     public int findPeakElement(int[] nums){
-//         int low = 0;
-//         int high = nums.length;
-//         while(low <= high){
-//             int mid = low + (high - low) / 2;
-//             if(nums[mid] < nums[mid + 1]){
-//                 low = mid ;
-//             }
-//             else{
-//                 high = mid; 
-//             }
-//         }
-//         return low;
-//     }
-// }
-
-
-class Solution {
-    public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < nums[mid + 1]) {
-                left = mid + 1;
-            } else {
-                right = mid;
+class Solution{
+    public int findPeakElement(int[] nums){
+        int low = 0;
+        int high = nums.length -1 ;
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] < nums[mid + 1]){
+                low = mid + 1 ;
+            }
+            else{
+                high = mid; 
             }
         }
-        
-        return left;
+        return low;
     }
 }
+
+
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//         int left = 0;
+//         int right = nums.length - 1;
+        
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] < nums[mid + 1]) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+        
+//         return left;
+//     }
+// }
